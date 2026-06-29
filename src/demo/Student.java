c402b2af160c8af82c8dd136996fab8dfbd3f351
Pathum Disanayaka <kumadisanayaka18@gmail.com>
2026-06-29 15:04:02 +0530

    Add classes for Sophomore, Junior, Senior, Graduate, Doctoral, and Masters students with level methods

--- src/demo/Student.java
+++ src/demo/Student.java
@@ -48,13 +48,108 @@ class Freshman extends UndergraduateStudent{
     }
 }
 
+class Sophomore extends UndergraduateStudent{
+
+    public Sophomore(String studentId, String name, int age, String faculty) {
+        super(studentId, name, age, faculty);
+    }
+    public void level(){
+        System.out.println("Second year");
+        undergraduateStudentInfo();
+    }
+}
+
+class Junior extends UndergraduateStudent{
+
+    public Junior(String studentId, String name, int age, String faculty) {
+        super(studentId, name, age, faculty);
+    }
+    public void level(){
+        System.out.println("Third year");
+        undergraduateStudentInfo();
+    }
+}
+
+class Senior extends UndergraduateStudent{
+
+    public Senior(String studentId, String name, int age, String faculty) {
+        super(studentId, name, age, faculty);
+    }
+    public void level(){
+        System.out.println("Fourth year");
+        undergraduateStudentInfo();
+    }
+}
+
+class GraduateStudent extends Student{
+    String researchArea;
+
+    GraduateStudent(String researchArea, String studentId, String name, int age) {
+        super(studentId, name, age);
+        this.researchArea = researchArea;
+    }
+    
+    public void graduateStudentInfo(){
+        System.out.println("Research Area : "+researchArea);
+        getInfo();
+    }
+    
+    public void level(){
+        System.out.println("Graduate Level");
+    }
+}
+
+class DoctoralStudent extends GraduateStudent{
+    
+    public DoctoralStudent(String researchArea, String studentId, String name, int age) {
+        super(researchArea, studentId, name, age);
+    }
+    
+    public void level(){
+        System.out.println("Level : Doctaral Student");
+        graduateStudentInfo();
+    }
+}
+
+class MastersStudent extends GraduateStudent{
+    
+    public MastersStudent(String researchArea, String studentId, String name, int age) {
+        super(researchArea, studentId, name, age);
+    }
+    
+    public void level(){
+        System.out.println("Level : Master Student");
+        graduateStudentInfo();
+    }
+}
+
 class Demo{
     public static void main(String args[]){
+        System.out.println("----------------------------------");
         UndergraduateStudent u1 = new UndergraduateStudent("U001", "Kalum", 0, "IT");
         u1.undergraduateStudentInfo();
+        System.out.println("----------------------------------");
         Freshman f1 = new Freshman("U002", "Jagath", 19, "Art");
         f1.level();
+        System.out.println("----------------------------------");
+        Sophomore s1 = new Sophomore("U003", "Kawinda", 23, "Agry");
+        s1.level();
+        System.out.println("----------------------------------");
+        Junior j1 = new Junior("U005", "Sanduni", 21, "Agry");
+        j1.level();
+        System.out.println("----------------------------------");
+        Senior s2 = new Senior("U005", "Sanduni", 21, "Agry");
+        s2.level();
+        System.out.println("----------------------------------");
+        DoctoralStudent d1 = new DoctoralStudent("Cyber seacurity", "D001", "Pathum", 32);
+        d1.level();
+        System.out.println("----------------------------------");
+        MastersStudent m1 = new MastersStudent("North Buddhism", "M001", "Rev.Amitha", 29);
+        m1.level();
+        
     }
+    
+    
 }
 
 

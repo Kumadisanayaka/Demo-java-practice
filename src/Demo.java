a123c88238b55f03ac9a843f792e31dfc267220d
Pathum Disanayaka <kumadisanayaka18@gmail.com>
2026-07-08 04:32:00 +0530

    Refactor Demo.java by removing MyFrame and related components, and adding MyClass for array handling

--- src/Demo.java
+++ src/Demo.java
@@ -1,36 +1,14 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-
-class MyFrame extends JFrame{
-	JButton setButton;
-	JTextField txtDisplay;
-	MyFrame(){
-		setSize(300,300);
-		setTitle("MyFrame");
-		setDefaultCloseOperation(3);
-		setLocationRelativeTo(null);
-		setLayout(new FlowLayout());	
-		
-		txtDisplay=new JTextField(12);
-		txtDisplay.setFont(new Font("",1,25));
-		add(txtDisplay);
-		
-		setButton=new JButton("Set Title");
-		setButton.setFont(new Font("",1,25));
-		setButton.addActionListener(new SetButtonActionListener());
-		add(setButton);
-	}
-        class SetButtonActionListener implements ActionListener{
-	public void actionPerformed(ActionEvent evt){
-		String title=txtDisplay.getText();
-		setTitle(title);
-	}
+class MyClass{
+	long[] ar=new long[1000000];
 }
-}
-
 class Demo{
 	public static void main(String args[]){
-		new MyFrame().setVisible(true);
+		System.out.println("Start main");
+		MyClass[] ar=new MyClass[100000];
+		for(int i=0; i<100000; i++){
+			System.out.println("i : "+i);
+			ar[i]=new MyClass();
+		}
+		System.out.println("End main");
 	}
 }

8d7fde83310f60447f842a6cd43cc3ffe6e6248f
Pathum Disanayaka <kumadisanayaka18@gmail.com>
2026-07-14 21:37:59 +0530

    Refactor List class to implement add, remove, size, and printList methods, replacing Queue class functionality

--- src/Demo.java
+++ src/Demo.java
@@ -6,94 +6,124 @@ class Node{
         this.data = data;
     }
 }
-class Queue{
-    private Node front;
+
+class List{
+    Node first;
     
-    //add data
+    //add(data)
     public void add(int data){
         Node n1 = new Node(data);
         if(isEmpty()){
-            front=n1;
+            first=n1;
         }else{
-            Node lastNode = front;
+            Node lastNode = first;
             while(lastNode.next!=null){
-                lastNode=lastNode.next;
+                lastNode = lastNode.next;
             }
-            lastNode.next = n1;
+            lastNode.next=n1;
+        } 
+    }
+    //add(index,data)
+    public void add(int index,int data){
+        Node n1 = new Node(data);
+        if(index==0){
+            n1.next = first;
+            first = n1;
+        }else{
+            if(index>=0 && index <= size()){
+                Node temp = first;
+                int count = 0;
+                while(count < index-1){
+                    count++;
+                    temp = temp.next;
+                }
+                n1.next = temp.next;
+                temp.next = n1;
+            }
         }
     }
-    //remove data
-    public void remove(){
-        if(front!=null){
-            front = front.next;
-        }
-    }
-    //isEmpty
-    public boolean isEmpty(){
-        return front==null;
-    }
-    //peek
-    public int peek(){
-        return isEmpty()?-1:front.data;
+    //addFirst
+    public void addFirst(int data){
+        add(0,data);
     }
     
-    //poll
-    public int poll(){
-        if(isEmpty()){
-            return -1;
-        }
-        int topData = front.data;
-        remove();
-        return topData;
+    //addLast
+    public void addLast(int data){
+        add(size(),data);
     }
-    //printQueue
-    public void printQueue(){
-        Node temp = front;
-        System.out.print("[");
-        while(temp!=null){
-            System.out.print(temp.data+", ");
-            temp=temp.next;
+    
+    //remove(index)
+    public void remove(int index){
+        if(index >=0 && index <size()){
+            if(index==0){
+                first=first.next;
+            }else{
+                int count = 0;
+                Node temp = first;
+                while(count < index-1){
+                    count++;
+                    temp=temp.next;
+                }
+                temp.next = temp.next.next;
+            }
         }
-        System.out.print(isEmpty()?"empty]":"\b\b]");
     }
+    //removeFirst
+    public void removeFirst(){
+        remove(0);
+    }
+    
+    //removeLast
+    public void removeLast(){
+        remove(size()-1);
+    }
+    
     //size
     public int size(){
         int count = 0;
-        Node temp = front;
+        Node temp = first;
         while(temp!=null){
             count++;
-            temp = temp.next;
+            temp=temp.next;
         }
         return count;
     }
-    
-    //indexOf
-    public int indexOf(int data){
-        int index = 0;
-        Node temp = front;
-        while(temp!=null){
-            if(temp.data==data){
-                return index;
-            }
-            index++;
-            temp = temp.next;
-        }
-        return index;
+    //isEmpty
+    public boolean isEmpty(){
+        return first==null;
     }
     
+    //printList
+    public void printList(){
+        Node temp = first;
+        System.out.print("[");
+        while(temp!=null){
+            System.out.print(temp.data+", ");
+            temp = temp.next;
+        }
+        System.out.print(isEmpty()?"empty]":"\b\b]");
+    }
 }
+ 
 class Demo{
     public static void main(String args[]){
-       Queue q1 = new Queue();
-       q1.add(10);
-       q1.add(20);
-       q1.add(30);
-       q1.add(40);
-       q1.add(50);
-       q1.printQueue();
+       List l1 = new List();
+       l1.add(10);
+       l1.add(20);
+       l1.add(30);
+       l1.add(40);
+       l1.add(50);
+       l1.add(60);
+       l1.printList();
+       l1.add(2, 25);
         System.out.println("");
-        System.out.println("size : "+q1.size());
-       q1.printQueue();
-        System.out.println("indexOf : "+q1.indexOf(20));
+       l1.addFirst(8);
+       l1.printList();
+       l1.remove(3);
+        System.out.println("");
+       l1.removeFirst();
+       l1.removeLast();
+       l1.printList();
+       
     }	
 }

7050cf1a30fcf06994b6ad237a762e243d3ed800
Pathum Disanayaka <kumadisanayaka18@gmail.com>
2026-07-16 21:15:13 +0530

    Refactor Demo.java to implement Alarm, Display, and SmsSender classes for water level monitoring

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -1,39 +1,70 @@
 package demo;
-class A{
-    int a;
-    A(){
-        System.out.println("A()");
-    }
-    A(int i){
-        System.out.println("A(int)");
-    }
-    A(int i, int j){
-        System.out.println("A(int,int)");
-    }
-    public void printA(){
-        System.out.println("print A : "+a);
+
+import java.util.Random;
+
+class Alarm{
+    public void operateAlarm(int waterLevel){
+        System.out.println(waterLevel>=50?"Alarm ON":"Alarm OFF");
     }
 }
-class B extends A{
-    int b;
-    B(){
-        super(); //Compiler inserts at compile time
-        System.out.println("B()");
+class Display{
+    public void display(int waterLevel){
+        System.out.println("Water Level : "+waterLevel);
     }
-    B(int i){
-        super(); //Compiler inserts at compile time
-        System.out.println("B(int)");
+}
+
+class SmsSender{
+    public void smsSender(int waterLevel){
+        System.out.println(waterLevel>=50?"Warning : "+waterLevel:"Normal : "+waterLevel);
     }
-    B(int i, int j){
-        super(); //Compiler inserts at compile time
-        System.out.println("B(int,int)");
+}
+
+class ControllRoom{
+    private Alarm alarm;
+    private Display display;
+    private SmsSender smsSender;
+    
+    private int waterLevel;
+    
+    public void addAlarm(Alarm alarm){
+        this.alarm = alarm;
     }
-    public void printB(){
-        System.out.println("print B : "+b);
+    
+    public void addDisplay(Display display){
+        this.display = display;
+    }
+    
+    public void addSmsSender(SmsSender smsSender){
+        this.smsSender = smsSender;
+    }
+    
+    public void setWaterLevel(int waterLevel){
+        if(this.waterLevel != waterLevel){
+            this.waterLevel = waterLevel;
+        }
+        notifyDevice();
+    }
+    
+    public void notifyDevice(){
+        alarm.operateAlarm(waterLevel);
+        display.display(waterLevel);
+        smsSender.smsSender(waterLevel);
     }
 }
 class Demo{
-    public static void main(String[] args) {
-        new B();
+    public static void main(String[] args) throws InterruptedException {
+        ControllRoom controllRoom = new ControllRoom();
+        controllRoom.addAlarm(new Alarm());
+        controllRoom.addDisplay(new Display());
+        controllRoom.addSmsSender(new SmsSender());
+        
+        Random r = new Random();
+        while(true){
+            int waterLevel = r.nextInt(101);
+            controllRoom.setWaterLevel(waterLevel);
+            Thread.sleep(1000);
+                    
+        }
     }   
 }
+

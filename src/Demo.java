e291c026a2462f68d45e3b426364bfd3dba9473d
Pathum Disanayaka <kumadisanayaka18@gmail.com>
2026-08-23 15:58:20 +0530

    Refactor Vehicle interface and remove WaterLevelObserver implementation; add Car class with park and start methods

--- src/Demo.java
+++ src/Demo.java
@@ -1,59 +1,22 @@
-import java.util.*;
-interface WaterLevelObserver{
-	abstract public void update(int waterLevel);
-}
-class Splitter implements WaterLevelObserver{
-	public void update(int waterLevel){
-		System.out.println(waterLevel>=75 ? "Splitter ON":"Splitter OFF");
-	}	
-}
-class Alarm implements WaterLevelObserver{
-	public void update(int waterLevel){
-		System.out.println(waterLevel>=50 ? "Alarm ON":"Alarm OFF");
+interface Vehicle{
+	default public void park(){  //Legal, since JDK1.8
+		System.out.println("default implementation..");
 	}
+	public void start();//abstract
 }
-class Display implements WaterLevelObserver{
-	public void update(int waterLevel){
-		System.out.println("WaterLevel : "+waterLevel);
+class Car implements Vehicle{
+	public void start(){
+		System.out.println("Car start...");
 	}
-}
-class SMSSender implements WaterLevelObserver{
-	public void update(int waterLevel){
-		System.out.println("Sending water level : "+waterLevel);
-	}
-}
-class ControlRoom{
-	private ArrayList <WaterLevelObserver>observerList=new ArrayList<>();
-	
-	private int waterLevel;
-	
-	public void addWaterLevelObserver(WaterLevelObserver ob){
-		observerList.add(ob);
-	}
-	public void setWaterLevel(int waterLevel){
-		if(this.waterLevel!=waterLevel){
-			this.waterLevel=waterLevel;
-		}
-		for(WaterLevelObserver ob : observerList){
-			ob.update(waterLevel);
-		}
-	}
+        
+        public void park(){
+            System.out.println("car parking...");
+        }
 }
 class Demo{	
 	public static void main(String args[]){
-		ControlRoom controlRoom=new ControlRoom();
-		controlRoom.addWaterLevelObserver(new Alarm());
-		controlRoom.addWaterLevelObserver(new Alarm());
-		controlRoom.addWaterLevelObserver(new Display());
-		controlRoom.addWaterLevelObserver(new SMSSender());
-		controlRoom.addWaterLevelObserver(new SMSSender());
-		controlRoom.addWaterLevelObserver(new Splitter());
-		Random r=new Random();
-		while(true){
-			int waterLevel=r.nextInt(101); //0 to 100
-			controlRoom.setWaterLevel(waterLevel);
-			try{Thread.sleep(1000);}catch(Exception ex){}
-			System.out.println();
-		}	
+		Vehicle v1=new Car();
+		v1.park();
+		v1.start();
 	}
 }

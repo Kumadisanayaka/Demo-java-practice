c410680ed2c2446736658e661c47022a8cdb8597
Pathum Disanayaka <kumadisanayaka18@gmail.com>
2026-08-23 15:13:18 +0530

    Add WaterLevelObserver implementation and ControlRoom class to manage observers

--- src/Demo.java
+++ src/Demo.java
@@ -1,24 +1,59 @@
-abstract class Vehicle{ 
-	abstract public void park();
-	abstract public void start();
-	public void stop(){
+import java.util.*;
+interface WaterLevelObserver{
+	abstract public void update(int waterLevel);
+}
+class Splitter implements WaterLevelObserver{
+	public void update(int waterLevel){
+		System.out.println(waterLevel>=75 ? "Splitter ON":"Splitter OFF");
+	}	
+}
+class Alarm implements WaterLevelObserver{
+	public void update(int waterLevel){
+		System.out.println(waterLevel>=50 ? "Alarm ON":"Alarm OFF");
+	}
+}
+class Display implements WaterLevelObserver{
+	public void update(int waterLevel){
+		System.out.println("WaterLevel : "+waterLevel);
+	}
+}
+class SMSSender implements WaterLevelObserver{
+	public void update(int waterLevel){
+		System.out.println("Sending water level : "+waterLevel);
+	}
+}
+class ControlRoom{
+	private ArrayList <WaterLevelObserver>observerList=new ArrayList<>();
 	
+	private int waterLevel;
+	
+	public void addWaterLevelObserver(WaterLevelObserver ob){
+		observerList.add(ob);
+	}
+	public void setWaterLevel(int waterLevel){
+		if(this.waterLevel!=waterLevel){
+			this.waterLevel=waterLevel;
+		}
+		for(WaterLevelObserver ob : observerList){
+			ob.update(waterLevel);
+		}
 	}
 }
-class Car extends Vehicle{}
-class Van extends Vehicle{
-	public void park(){
-		
+class Demo{	
+	public static void main(String args[]){
+		ControlRoom controlRoom=new ControlRoom();
+		controlRoom.addWaterLevelObserver(new Alarm());
+		controlRoom.addWaterLevelObserver(new Alarm());
+		controlRoom.addWaterLevelObserver(new Display());
+		controlRoom.addWaterLevelObserver(new SMSSender());
+		controlRoom.addWaterLevelObserver(new SMSSender());
+		controlRoom.addWaterLevelObserver(new Splitter());
+		Random r=new Random();
+		while(true){
+			int waterLevel=r.nextInt(101); //0 to 100
+			controlRoom.setWaterLevel(waterLevel);
+			try{Thread.sleep(1000);}catch(Exception ex){}
+			System.out.println();
+		}	
 	}
-}
-class Bus extends Vehicle{
-	public void park(){
-		
-	}
-	public void start(){
-		
-	}
-}
-abstract class Lorry extends Vehicle{
-
 }
